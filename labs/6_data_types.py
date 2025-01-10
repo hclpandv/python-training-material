2db1a5fcd1f1f4ecad57c0d2a0902a5cabb3fe90
Vikas Pandey <vikaspandey@Vikass-MacBook-Pro.local>
2025-01-10 13:37:53 +0100

    datatypes

---
+++ labs/6_data_types.py
@@ -0,0 +1,19 @@
+num = 5
+my_string = "Hello World" #string
+my_float = 20.5	#float	
+my_list = ["apple", "banana", "cherry"]	#list	
+my_tuple = ("apple", "banana", "cherry")	#tuple	
+my_range = range(6)	#range	
+my_dist = {"name" : "John", "age" : 36}	#dict	
+my_set = {"apple", "banana", "cherry"}	#set	
+my_bool = True	#bool	
+my_bytes = b"Hello"	#bytes	
+my_barray = bytearray(5)	#bytearray	
+my_mem = memoryview(bytes(5))	#memoryview	
+my_none = None
+
+print(type(num))
+print(type(my_float))
+print(type(my_tuple))
+print(type(my_set))
+print(type(my_none))
